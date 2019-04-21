e0f661f247c26974d53e335ab8268e5f7b7fedf8
Leland Krych <Leland.Krych@gmail.com>
2019-04-21 11:44:49 -0700

    change the server message slightly for the udp example

--- socket_programming/udp/udp_server.py
+++ socket_programming/udp/udp_server.py
@@ -4,7 +4,7 @@ server_port = 12000
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('localhost', server_port))
 
-print("The server is ready to receive messages on port {port}".format(port=server_port))
+print("The UDP server is ready to receive messages on port {port}".format(port=server_port))
 while True:
   message, client_address = server_socket.recvfrom(2048)
   print("Received a message from client address: {client}".format(client=client_address))
