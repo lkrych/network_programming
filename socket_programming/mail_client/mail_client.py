13a59165c00533416bacf8cde178432f16dce1b5
Leland Krych <Leland.Krych@gmail.com>
2019-05-02 07:34:48 -0700

    establish tcp connection with mail server

--- socket_programming/mail_client/mail_client.py
+++ socket_programming/mail_client/mail_client.py
@@ -22,29 +22,35 @@ else:
   print("That's not a valid email, I will now self destruct.")
   exit(1)
 
+#check if email has an MX record using dnspython
+host = ""
 try:
   answers = dns.resolver.query(domain, 'MX')
   for rdata in answers:
     print('Host', rdata.exchange, 'has preference', rdata.preference)
+    if len(host) == 0:
+      host = rdata.exchange
 except Exception as e:
   print(e)
   print("There is no MX record for that email and")
   print("unfortunately the resolvers have turned on us. I will now self destruct.")
   exit(1)
-	      
 
-# server_name = "" 
-# server_port = 25
-# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#grab a record's host and try to make a TCP connection connection with it
 
-# #initialize the tcp connection between the client and server
-# client_socket.connect((server_name, server_port))
+server_name = host 
+server_port = 25
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# print("Welcome to Leland's command line email client! \n")
-# email = input("Please input the email address you want to send to:")
+#initialize the tcp connection between the client and server
+client_socket.connect((server_name, server_port))
+
+#begin communication with the mail server
+
 # client_socket.send(message.encode())
 # modified_message = client_socket.recv(1024)
 
 # print("Message from server: {message}".format(message=modified_message.decode()))
+
 # client_socket.close()
 
