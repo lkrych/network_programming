d97e2b2f587e2ea3acc82de77ae9ba664ad78e65
Leland Krych <Leland.Krych@gmail.com>
2019-05-02 08:10:10 -0700

    Add sample smpt communication to mail_client and make sure access to email host is correct

--- socket_programming/mail_client/mail_client.py
+++ socket_programming/mail_client/mail_client.py
@@ -29,7 +29,7 @@ try:
   for rdata in answers:
     print('Host', rdata.exchange, 'has preference', rdata.preference)
     if len(host) == 0:
-      host = rdata.exchange
+      host = rdata.exchange.to_text(omit_final_dot=True)
 except Exception as e:
   print(e)
   print("There is no MX record for that email and")
@@ -42,15 +42,42 @@ server_name = host
 server_port = 25
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#initialize the tcp connection between the client and server
+
 client_socket.connect((server_name, server_port))
 
 #begin communication with the mail server
+# sample SMTP communication
 
-# client_socket.send(message.encode())
-# modified_message = client_socket.recv(1024)
+# S: 220 <HOST DOMAIN>
+# C: HELO <CLIENT DOMAIN>
+# S: 250 Hello <CLIENT DOMAIN>, pleased to meet you
+# C: MAIL FROM: <leland.krych@gmail.com>
+# S: 250 leland.krych@gmail.com ... Sender ok
+# C: RCPT TO: <blah@example.com>
+# S: 250 blah@example.com ... Recipient ok
+# C: DATA
+# S: 354 Enter mail, end with "." on a line by itself
+# C: Here is a message
+# C: .
+# S: 250 Message accepted for delivery
+# C: QUIT
+# S: 221 <HOST DOMAIN> closing connection 
+greeting_from_server = client_socket.recv(1024)
+if greeting_from_server[:3] != '220':
+    print('220 reply not received from server. received', greeting_from_server.decode())
+    print('I cannot go on. I am embarassed. I will now self-destruct :(')
+    exit(1)
+print("Greeting from mail server: ", greeting_from_server.decode())
 
-# print("Message from server: {message}".format(message=modified_message.decode()))
+client_socket.send("HELO Leland's Simple Mail Client")
 
-# client_socket.close()
+handshake_from_server = client_socket.recv(1024)
+if handshake_from_server[:3] != '250':
+    print('250 reply not received from server. received', handshake_from_server.decode())
+    print('The mail server doesn\'t want to shake hands. I am embarassed.')
+    print('I will now self-destruct.')
+    exit(1)
+print("Handshake from mail server: ", handshake_from_server.decode())
+
+client_socket.close()
 
