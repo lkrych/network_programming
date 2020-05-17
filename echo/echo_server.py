8a7f766975bebf6efc5ccfecc340fcd1c9d92506
Leland Krych <leland.krych@gmail.com>
2020-05-17 08:34:05 -0700

    create some notes

--- echo/echo_server.py
+++ echo/echo_server.py
@@ -3,8 +3,11 @@ import socket
 HOST = '127.0.0.1'  # Standard loopback interface address (any traffic that is addressed to the loopback IP address is addressed to the same computer)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
+# because we are using the context manager type of socket, we don't have to call close()
+# AF_INET specifies IPv4 address family, SOCK_STREAM is the socket type for TCP.
+# bind() is used to associate the socket with a specific network interface
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, port))
+    s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
     with conn:
