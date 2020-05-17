5b33f5b8530c9764eff51939d23bbbd0e6ff41cf
Leland Krych <leland.krych@gmail.com>
2020-05-16 20:19:23 -0700

    create echo server

---
+++ echo/echo_server.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST = '127.0.0.1'  # Standard loopback interface address (any traffic that is addressed to the loopback IP address is addressed to the same computer)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, port))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print("Connected by", addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data) #send the data back
